a0b53533d6778dbf08801b4e97eed30f7fcde79f
OrcaPhan <patuananh11@gmail.com>
2025-10-22 22:32:35 +0700

    code luồng thuộc proccess

--- src/main/java/com/orca/pbl4/core/model/ThreadInfo.java
+++ src/main/java/com/orca/pbl4/core/model/ThreadInfo.java
@@ -1,4 +1,35 @@
 package com.orca.pbl4.core.model;
 
 public class ThreadInfo {
+    private final int tid;
+    private final String name;
+    private final String state;
+    private final long cpuTimeTicks;
+
+    public ThreadInfo(int tid, String name, String state, long cpuTimeTicks) {
+        this.tid = tid;
+        this.name = name;
+        this.state = state;
+        this.cpuTimeTicks = cpuTimeTicks;
+
+    }
+
+    //getter
+    public int getTid() {
+        return tid;
+    }
+    public String getName() {
+        return name;
+
+    }
+    public String getState() {
+        return state;
+    }
+    public long getCpuTimeTicks() {
+        return cpuTimeTicks;
+    }
+
+    public String toString() {
+        return "ThreadInfor{ tid=" + tid + ", name=" + name +", state=" + state + ", cpuTimeTicks=" + cpuTimeTicks + "}";
+    }
 }
