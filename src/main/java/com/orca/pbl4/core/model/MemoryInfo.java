bd5c901f6c625ddf2c73d4afb98437cffcc4a957
OrcaPhan <patuananh11@gmail.com>
2025-10-22 20:53:35 +0700

    tạo class memoryinfo để lưu trữ thông tin ram

--- src/main/java/com/orca/pbl4/core/model/MemoryInfo.java
+++ src/main/java/com/orca/pbl4/core/model/MemoryInfo.java
@@ -1,4 +1,12 @@
 package com.orca.pbl4.core.model;
 
 public class MemoryInfo {
+    private long total;
+    private long used;
+    private long swapUsed;
+
+    public double readMemInfo(){
+        return 0;
+    }
+
 }
