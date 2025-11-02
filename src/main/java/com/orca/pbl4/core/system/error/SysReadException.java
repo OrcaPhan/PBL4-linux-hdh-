7ea1558426849ae51ce64dca650e64d40bff43b9
OrcaPhan <patuananh11@gmail.com>
2025-11-02 15:30:27 +0700

    class báo lỗi khi đọc proc

--- src/main/java/com/orca/pbl4/core/system/error/SysReadException.java
+++ src/main/java/com/orca/pbl4/core/system/error/SysReadException.java
@@ -1,4 +1,6 @@
 package com.orca.pbl4.core.system.error;
 
-public class SysReadException {
+public class SysReadException extends RuntimeException{
+    public SysReadException(String message) { super(message);}
+    public SysReadException(String message, Throwable cause) { super(message, cause); }
 }
